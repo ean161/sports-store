2d2a2058a86f56aea4660890a37bc9b02da39248
ean161 <channelentity303@gmail.com>
2025-09-23 21:22:04 +0700

    init

---
+++ src/main/java/fcc/sportsstore/SportsStoreApplication.java
@@ -0,0 +1,13 @@
+package fcc.sportsstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportsStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportsStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fcc/sportsstore/SportsStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package fcc.sportsstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportsStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
