21b50f1da0612ba9a3524435f39fa5229675b217
ean161 <channelentity303@gmail.com>
2025-09-24 09:58:48 +0700

    docs: auth methods

--- src/main/java/fcc/sportsstore/controller/Auth.java
+++ src/main/java/fcc/sportsstore/controller/Auth.java
@@ -5,20 +5,33 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+/**
+ * Auth controller, login and register maps
+ */
 @Controller
 @RequestMapping("/auth")
-
-// auth/login, auth/register
 public class Auth {
 
+    /**
+     * Login map
+     * @param model Template model
+     * @return Login page
+     */
     @GetMapping("/login")
     public String login(Model model) {
+        // Notice for UI to show login components
         model.addAttribute("type", "login");
         return "pages/auth";
     }
 
+    /**
+     * Register map
+     * @param model Template model
+     * @return Register page
+     */
     @GetMapping("/register")
     public String register(Model model) {
+        // Notice for UI to show register components
         model.addAttribute("type", "register");
         return "pages/auth";
     }
