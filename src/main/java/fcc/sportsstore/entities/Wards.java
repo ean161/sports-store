2f070d7fdb9839dd30a58e3666e27efe91b462b5
ean161 <channelentity303@gmail.com>
2025-10-03 19:33:50 +0700

    feat: add provinces and wards

---
+++ src/main/java/fcc/sportsstore/entities/Wards.java
@@ -0,0 +1,31 @@
+package fcc.sportsstore.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor @AllArgsConstructor
+public class Wards {
+
+    @Id
+    @Column(name = "wards_id")
+    private String id;
+
+    private String name;
+
+    private int vtpReferrenceId;
+
+    @ManyToOne
+    @JoinColumn(name = "province_id")
+    private Province province;
+
+    public Wards(String id, String name, int vtpReferrenceId) {
+        this.id = id;
+        this.name = name;
+        this.vtpReferrenceId = vtpReferrenceId;
+    }
+}
---
+++ src/main/java/fcc/sportsstore/repositories/ProvinceRepository.java
@@ -0,0 +1,12 @@
+package fcc.sportsstore.repositories;
+
+import fcc.sportsstore.entities.Province;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ProvinceRepository extends JpaRepository<Province, String> {
+    Optional<Province> findByVtpReferrenceId(Integer id);
+}
