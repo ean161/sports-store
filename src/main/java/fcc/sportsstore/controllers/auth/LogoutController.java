0fe8b726ab0779fb40eeb8d774d5ea8dabc9b77c
NgoNhatQuy <QuyNN.CE190984@gmail.com>
2025-10-12 01:57:10 +0700

    feat: add logout

---
+++ src/main/java/fcc/sportsstore/controllers/auth/LogoutController.java
@@ -0,0 +1,30 @@
+package fcc.sportsstore.controllers.auth;
+
+import fcc.sportsstore.services.auth.LogoutService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/logout")
+public class LogoutController {
+
+    final private LogoutService logoutService;
+
+    public LogoutController(LogoutService logoutService) {
+        this.logoutService = logoutService;
+    }
+
+    @GetMapping
+    public String index(HttpServletRequest request, HttpServletResponse response) {
+        try {
+            logoutService.logout(request, response);
+        } catch (Exception e) {
+            System.err.println("Logout failed: " + e.getMessage());
+        }
+
+        return "redirect:/";
+    }
+}
