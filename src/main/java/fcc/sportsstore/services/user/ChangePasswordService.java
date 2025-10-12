53a593af58910c69d85cac9f6b40ec7dc4b96066
vincentyo2112 <YoTS.CE190435@gmail.com>
2025-10-12 23:55:18 +0700

    feat: add change password

---
+++ src/main/java/fcc/sportsstore/services/user/ChangePasswordService.java
@@ -0,0 +1,49 @@
+package fcc.sportsstore.services.user;
+
+import fcc.sportsstore.entities.User;
+import fcc.sportsstore.services.UserService;
+import fcc.sportsstore.utils.HashUtil;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Service;
+import fcc.sportsstore.utils.Validate;
+
+@Service
+public class ChangePasswordService {
+
+    final private UserService userService;
+
+    public ChangePasswordService(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Transactional
+    public void changePassword(HttpServletRequest request,
+                               String oldPassword,
+                               String newPassword,
+                               String newPasswordConfirm) {
+        Validate validate = new Validate();
+        User caller = userService.getUserFromSession(request);
+
+        if (newPassword == null || newPassword.trim().isEmpty()) {
+            throw new RuntimeException("New password cannot be empty");
+        } if (!validate.isValidPassword(newPassword)) {
+            throw new RuntimeException("New password length must be from 6 - 30 characters, contains a special character.");
+        } if (!newPassword.equals(newPasswordConfirm)) {
+            throw new RuntimeException("New passwords and new password confirm do not match");
+        } if (oldPassword == null || oldPassword.trim().isEmpty()) {
+            throw new RuntimeException("Old password cannot be empty");
+        }
+
+        HashUtil hash = new HashUtil();
+        String hashedOldPassword = hash.md5(oldPassword);
+        String hashedNewPassword = hash.md5(newPassword);
+
+        if (!caller.getPassword().equals(hashedOldPassword)) {
+            throw new RuntimeException("Old passwords do not match");
+        }
+
+        caller.setPassword(hashedNewPassword);
+        userService.save(caller);
+    }
+}
