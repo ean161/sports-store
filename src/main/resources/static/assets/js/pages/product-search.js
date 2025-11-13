4b427f88e852cf55691ae618842b0c57b4882da8
ean161 <channelentity303@gmail.com>
2025-11-13 13:32:37 +0700

    style: re-design header components position

--- src/main/resources/static/assets/js/pages/product-search.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function () {
-    $("#search-bar").on("change", async function (event) {
-        let key = $(this).val();
-
-        window.location.href = `/search?keyword=${key}`;
-    });
-});
