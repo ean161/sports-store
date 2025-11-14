5e8a5356889e666b93c4099f53691b914a5a695b
ean161 <channelentity303@gmail.com>
2025-11-14 12:58:17 +0700

    chore: update prod quantity on order status changed in manager

--- src/main/java/fcc/sportsstore/services/manager/ManageOrderService.java
+++ src/main/java/fcc/sportsstore/services/manager/ManageOrderService.java
@@ -1,28 +1,37 @@
 package fcc.sportsstore.services.manager;
 
-import fcc.sportsstore.entities.Manager;
-import fcc.sportsstore.entities.Pack;
-import fcc.sportsstore.entities.ProductCollection;
+import fcc.sportsstore.entities.*;
 import fcc.sportsstore.services.ManagerService;
 import fcc.sportsstore.services.PackService;
+import fcc.sportsstore.services.ProductPropertySnapshotService;
+import fcc.sportsstore.services.ProductQuantityService;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 @Service("managerManageOrderService")
 public class ManageOrderService {
 
     private final PackService packService ;
+
     private final ManagerService managerService;
 
-    public ManageOrderService(PackService packService, ManagerService managerService) {
+    private final ProductQuantityService productQuantityService;
+
+    private final ProductPropertySnapshotService productPropertySnapshotService;
+
+    public ManageOrderService(PackService packService, ManagerService managerService, ProductQuantityService productQuantityService, ProductPropertySnapshotService productPropertySnapshotService) {
         this.packService = packService;
         this.managerService = managerService;
+        this.productQuantityService = productQuantityService;
+        this.productPropertySnapshotService = productPropertySnapshotService;
     }
 
     public Page<Pack> list(String search, Pageable pageable) {
@@ -54,7 +63,42 @@ public class ManageOrderService {
         }
 
         Pack pack = packService.getById(id);
+        String oldStatus = pack.getStatus();
         pack.setManager(managerService.getManagerFromSession(request));
         pack.setStatus(status);
+
+        String stockAffect = "no";
+
+        if (!oldStatus.equals("CANCELLED") && status.equals("CANCELLED")) {
+            stockAffect = "sub";
+        }
+
+        if (oldStatus.equals("CANCELLED") && !status.equals("CANCELLED")) {
+            stockAffect = "add";
+        }
+
+        if (!stockAffect.equals("no")) {
+            System.out.println("STOCK AFFECT: " + stockAffect);
+            for (ProductSnapshot snap : pack.getProductSnapshots()) {
+                List<ProductPropertySnapshot> snapProps = snap.getProductPropertySnapshots();
+                Set<ProductPropertyData> propData = new HashSet<>();
+                for (ProductPropertySnapshot snapPropsItem : snapProps) {
+                    propData.add(productPropertySnapshotService.toPropertyData(snapPropsItem));
+                }
+
+                ProductQuantity pQuantity = productQuantityService.getByProperties(propData);
+
+
+                if (stockAffect.equals("sub")) {
+                    pQuantity.setAmount(pQuantity.getAmount() + snap.getQuantity());
+                } else {
+                    if (pQuantity.getAmount() - snap.getQuantity() >= 0) {
+                        pQuantity.setAmount(pQuantity.getAmount() - snap.getQuantity());
+                    } else {
+                        pQuantity.setAmount(0);
+                    }
+                }
+            }
+        }
     }
 }
